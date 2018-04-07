67cb031d73e012215257af02be245cb3087a0b9c
u0jin <yujin5836@gmail.com>
2018-04-07 23:05:34 +0900

    combobox updated
    commoBox updated

--- C#/_DB연동(0407)/_DB연동(0327)/Form1.Designer.cs
+++ C#/_DB연동(0407)/_DB연동(0327)/Form1.Designer.cs
@@ -28,23 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.button = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // button
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.button1.Location = new System.Drawing.Point(371, 102);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "검색";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button.Location = new System.Drawing.Point(371, 102);
+            this.button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(86, 29);
+            this.button.TabIndex = 0;
+            this.button.Text = "검색";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
             // 
             // textBox1
             // 
@@ -53,6 +54,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(278, 25);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // dataGridView1
@@ -65,17 +67,28 @@
             this.dataGridView1.Size = new System.Drawing.Size(400, 296);
             this.dataGridView1.TabIndex = 2;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(520, 562);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -84,9 +97,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button;
         private System.Windows.Forms.TextBox textBox1;
         public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- C#/_DB연동(0407)/_DB연동(0327)/Form1.cs
+++ C#/_DB연동(0407)/_DB연동(0327)/Form1.cs
@@ -10,41 +10,94 @@ using System.Windows.Forms;
 
 namespace _DB연동_0327_
 {
+   
     public partial class Form1 : Form
     {
         Con_Database db;
+       int index=1; //전역변수 (Column의 index 선택하는)
+                    //Default 검색은 NAME
 
         public Form1()
         {
-     
-
             InitializeComponent();
             db = new Con_Database(this);
 
             db.ConnectDB();
-
-
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        //index==0 -> ID순
+        //index==1 -> NAME순
+        //index==2 -> E-Mail순
+        //index==3 -> Address순
+        //index==4 -> PhoneNumber순
+        private void button_Click(object sender, EventArgs e)  
         {
-            
-            string Mysql = "SELECT * FROM data where name LIKE '%" + textBox1.Text + "%' ";
+            string Mysql="";
+
+            if (index == 0)
+            {
+                Mysql = "SELECT * FROM data where id LIKE '%" + textBox1.Text + "%' ";
+            }
+            else if(index==1)
+            {
+                Mysql = "SELECT * FROM data where name LIKE '%" + textBox1.Text + "%' ";
+            }
+            else if (index == 2)
+            {
+                Mysql = "SELECT * FROM data where email LIKE '%" + textBox1.Text + "%' ";
+            }
+            else if (index == 3)
+            {
+                Mysql = "SELECT * FROM data where address LIKE '%" + textBox1.Text + "%' ";
+            }
+            else if (index == 4)
+            {
+                Mysql = "SELECT * FROM data where phonenumber LIKE '%" + textBox1.Text + "%' ";
+            }
+
             DataTable dt = db.GetDBTable(Mysql);
             dataGridView1.DataSource = dt;
             db.CloseDB();
 
 
         }
+     
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == '\r')
             {
-                 button1_Click(sender,e);
-               // button1_Click(null, null); 왜 둘이 같은 동작을 하는걸까요???
+                button_Click(sender, e);    //이름 기준
+               // button_Click(null, null); 왜 둘이 같은 동작을 하는걸까요???
             }
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { //간지용
+            button_Click(sender, e);        //이름 기준
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+              //  MessageBox(comboBox1.SelectedIndex.ToString());
+             //   MessageBox.Show(comboBox1.SelectedIndex.ToString());// 인덱스 출력
+                index = comboBox1.SelectedIndex;
+                //this.itemSelected = comboDropDown.SelectedItem as string;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] data = { "ID", "Name", "E-mail", "Address", "Phone_number" };
+            //comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Text = "Column";
+            comboBox1.Items.AddRange(data); //배열
+    
+        }
+
 
         //특정 키 버튼 처리-키를 처음 누를때 처리되는것
       
