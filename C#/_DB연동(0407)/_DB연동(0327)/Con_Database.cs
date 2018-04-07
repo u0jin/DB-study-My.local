de7aef10ea865c6e8c32f45bf55ee6a58d6e39d8
u0jin <yujin5836@gmail.com>
2018-04-07 21:31:59 +0900

    key event updated1
    key event excute

---
+++ C#/_DB연동(0407)/_DB연동(0327)/Con_Database.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace _DB연동_0327_
+{
+    class Con_Database
+    {
+        static String constring1 = "Server=localhost;Database=ujindb;Uid=root;Pwd=tryit5826;SslMode=none;Charset=utf8;";
+
+        MySqlConnection conn = new MySqlConnection(constring1); //데이터베이스와 연결을 담당하는 객체 생성
+
+         Form1 frm1;
+
+
+        MySqlDataAdapter adapter;
+
+
+        public string sConnstring = "";
+
+        public Con_Database(Form1 _frm1)
+        {
+            frm1 = _frm1;
+
+        }
+
+        public void ConnectDB()
+        {
+            //GridVeiw1에 보여줌
+
+            try
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM data ORDER BY id";
+              
+                adapter = new MySqlDataAdapter(sql, conn);
+                DataSet ds = new DataSet();
+                
+                adapter.Fill(ds,"data");
+                frm1.dataGridView1.DataSource = ds.Tables[0];
+
+                conn.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.ToString());
+                conn.Close();
+            }
+
+        }
+
+        public void CloseDB()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+
+
+        public DataTable GetDBTable(string sql)
+        {
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+            MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
+            DataTable dt = new DataTable();
+
+            adapter.Fill(dt);
+            return dt;
+
+        }
+
+       
+        public void load_Research()
+        {
+            try
+            {
+                conn.Open();
+                string sql = "SELECT * FROM data where name LIKE '%"+frm1.Text+"%' ";
+
+                adapter = new MySqlDataAdapter(sql, conn);
+                DataSet ds4 = new DataSet();
+
+                adapter.Fill(ds4, "data");
+                frm1.dataGridView1.DataSource = ds4.Tables[0];
+           
+
+               conn.Close();
+            }
+
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.ToString());
+                conn.Close();
+            }
+
+            }
+        }
+
+    }
---
+++ C#/_DB연동(0407)/_DB연동(0327)/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _DB연동_0327_
+{
+    public partial class Form1 : Form
+    {
+        Con_Database db;
+
+        public Form1()
+        {
+     
+
+            InitializeComponent();
+            db = new Con_Database(this);
+
+            db.ConnectDB();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string Mysql = "SELECT * FROM data where name LIKE '%" + textBox1.Text + "%' ";
+            DataTable dt = db.GetDBTable(Mysql);
+            dataGridView1.DataSource = dt;
+            db.CloseDB();
+
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                 button1_Click(sender,e);
+               // button1_Click(null, null); 왜 둘이 같은 동작을 하는걸까요???
+            }
+        }
+
+        //특정 키 버튼 처리-키를 처음 누를때 처리되는것
+      
+
+    
+    }
+}
